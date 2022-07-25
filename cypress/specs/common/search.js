5d6608d3673f9a993ef770f3b31fce8349b6b530
alexandruminnio <mutescul.alexandru@gmail.com>
2022-07-25 10:12:41 +0300

    Add search rooms test

--- cypress/specs/common/search.js
+++ cypress/specs/common/search.js
@@ -1,11 +1,60 @@
+const getIframeDom = iframeCssSelector =>
+  cy
+    .get(iframeCssSelector, { timeout: 10000 })
+    .should($iframe => {
+      expect($iframe).to.have.lengthOf(1);
+      expect($iframe[0].contentDocument.body).to.not.be.empty;
+    })
+    .its('0.contentDocument.body')
+    .then(cy.wrap);
+
 When("I select check in date {string}", (checkInDate) => {
+      getIframeDom('iframe[title="Wix Hotels"]')
+        .find('#check-in')
+        .click();
+      cy.wait(2000)
+  getIframeDom('iframe[title*="tpapopup"]').find(`button[aria-label="${checkInDate}"]`)
+    .click()
+});
 
-    cy.get('iframe[title="Wix Hotels"]')
-        .then(($iframe) => {
-            const $body = $iframe.contents().find('body')
+And("I select check out date {string}", (checkOutDate) => {
+  getIframeDom('iframe[title="Wix Hotels"]')
+    .find('#check-out')
+    .click();
+  cy.wait(2000)
 
-            cy.wrap($body)
-                .find('button[aria-label="29, Friday July 2022"]')
-                .click()
-        })
+  getIframeDom('iframe[title*="tpapopup"]').find('button[aria-label="29, Monday August 2022"]')
+    .click()
+  cy.wait(2000)
+})
+
+And("I select 2 adults per room", () => {
+  getIframeDom('iframe[title="Wix Hotels"]')
+    .find('a[aria-label="Increment"]').eq(0)
+    .click();
+  cy.wait(2000)
+})
+
+And("I search for corresponding rooms", () => {
+  getIframeDom('iframe[title="Wix Hotels"]')
+    .find('button[wix-bi="SEARCH"]')
+    .click();
+  cy.wait(2000)
+})
+
+let checkThatRoomTypeIsDisplayed = (index, roomType) => {
+  getIframeDom('iframe[title="Book a Room"]')
+    .find('li[ng-repeat*="room in rooms"]')
+    .eq(index)
+    .find('[wix-bi="ROOM_DETAILS"]')
+    .find("span")
+    .then($text => {
+      expect($text.text()).to.contain(roomType)
+    })
+}
+
+Then("I check that search results for available rooms is shown", () => {
+  checkThatRoomTypeIsDisplayed(0, "Standard Suite")
+  checkThatRoomTypeIsDisplayed(1, "Cottage")
+  checkThatRoomTypeIsDisplayed(2, "Classic App")
 })
--- cypress/specs/search_for_room.feature
+++ cypress/specs/search_for_room.feature
@@ -3,3 +3,7 @@ Feature: Home page
   Scenario: Search
     Given I open home page
     When I select check in date "29, Friday July 2022"
+    And I select check out date "29, Monday August 2022"
+    And I select 2 adults per room
+    And I search for corresponding rooms
+    Then I check that search results for available rooms is shown
